eb49504407b67d470533a07cc44709921d4c883a
saragojic <sa20b004@technikum-wien.at>
2022-10-02 21:40:04 +0200

    Step 10

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-/*console.log("Hello World")*/
-
+// Model
+const notes = [
+    // "Note 1",
+    // "Note 2",
+    // "Note 3"
+    // ...
+  ];
+  
+  // View
+  // see HTML
+  function buildLIItem(note) {
+    const item = document.createElement("li");
+    item.textContent = note;
+    item.addEventListener("click", handleClickLIItem);
+    return item;
+  }
+  
+  // Controller
 /*Es gibt für DOMContentLoaded keine entsprechende Objekteigenschaft, dass heißt, eine Überwachung dieses Ereignisses muss immer mit addEventListener erfolgen.
 Verwenden Sie DOMContentLoaded anstelle von load, damit Skripte schon geladen werden, bevor alle Bilder und anderen Mediendateien fertig geladen sind. */
 document.addEventListener("DOMContentLoaded", function () {
@@ -25,16 +41,15 @@ function handleKeyDown(event) {
     list.removeChild(event.target);
   }
 
-  function add() {
-  const input = document.getElementById("text");
-  const note = input.value;
-  if (note) {
-    const list = document.getElementById("list");
-    const item = document.createElement("li");
-    item.textContent = note;
-    item.addEventListener("click", handleClickLIItem);
-    list.appendChild(item);
-    input.value = "";
-    input.focus();
-  }
+function add() {
+    const input = document.getElementById("text");
+    const note = input.value;
+    if (note) {
+        const list = document.getElementById("list");
+        const item = buildLIItem(note);
+        list.appendChild(item);
+        notes.push(note);
+        input.value = "";
+        input.focus();
+    }
 }
