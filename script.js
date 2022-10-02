4d152b326511ddcd6e6fed21c0aa96db586b2cc0
saragojic <sa20b004@technikum-wien.at>
2022-10-02 21:19:20 +0200

    Step 6

--- script.js
+++ script.js
@@ -2,7 +2,21 @@
 const button = document.getElementById("add");
 button.addEventListener("click", handleClick);
 
+const input = document.getElementById("text");
+input.addEventListener("keydown", handleKeyDown);
+
+
 function handleClick(/* event */) {
+    add();
+}
+
+function handleKeyDown(event) {
+    if (event.key === "Enter") {
+      add();
+    }
+  }
+
+  function add() {
   const input = document.getElementById("text");
   const note = input.value;
   if (note) {
