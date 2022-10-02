199609bee824cb42bfa94f3113fa1a08e8b13c7b
saragojic <sa20b004@technikum-wien.at>
2022-10-02 22:05:00 +0200

    Step 13

--- script.js
+++ script.js
@@ -7,12 +7,29 @@ const notes = [
   
   // View
   // see HTML
-  function buildLIItem(note) {
+function buildLIItem(note) {
     const item = document.createElement("li");
-    item.textContent = note;
-    item.addEventListener("click", handleClickLIItem);
+    item.id = note.id;
+
+    const article = document.createElement("article");
+    const title = document.createElement("header");
+    title.textContent = note.title;
+    const text = document.createElement("p");
+    text.textContent = note.text;
+
+    const controls = document.createElement("div");
+    const button = document.createElement("button");
+    button.textContent = "Delete";
+    button.addEventListener("click", handleClickDelete(note.id));
+    controls.appendChild(button);
+
+    article.appendChild(title);
+    article.appendChild(text);
+    article.appendChild(controls);
+    item.appendChild(article);
+   
     return item;
-  }
+}
   
   // Controller
 /*Es gibt für DOMContentLoaded keine entsprechende Objekteigenschaft, dass heißt, eine Überwachung dieses Ereignisses muss immer mit addEventListener erfolgen.
@@ -26,20 +43,40 @@ function handleClick(/* event */) {
     add();
 }
 
-  function handleClickLIItem(event) {
-    const list = document.getElementById("list");
-    list.removeChild(event.target);
-  }
+function handleClickLIItem(event) {
+    function handleClickDelete(id) {
+        return function () {
+          const item = document.getElementById(id);
+          const list = document.getElementById("list");
+          list.removeChild(item);
+          const pos = notes.findIndex((note) => note.id === id);
+          notes.splice(pos, 1);
+        };
+    }
+}
 
 function add() {
-    const input = document.getElementById("text");
-    const note = input.value;
-    if (note) {
+    const title = document.getElementById("title");
+    const text = document.getElementById("text");
+    if (title.value || text.value) {
         const list = document.getElementById("list");
+        const note = createNote(title.value, text.value);
         const item = buildLIItem(note);
         list.appendChild(item);
         notes.push(note);
-        input.value = "";
-        input.focus();
+        title.value = "";
+        text.value = "";
     }
+}
+
+function createNote(title, text) {
+    const id = generateId(title, text);
+    return { id, title, text };
+}
+
+  
+function generateId(title, text, length = 10) {
+    return CryptoJS.SHA256(title + text + new Date())
+      .toString()
+      .substring(0, length);
 }
