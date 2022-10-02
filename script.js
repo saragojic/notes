3462a75a2909dca0a379dd75b2778c06f465a004
saragojic <sa20b004@technikum-wien.at>
2022-10-02 18:47:34 +0200

    Step4 script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+/*console.log("Hello World")*/
+const button = document.getElementById("add");
+button.addEventListener("click", handleClick);
+
+function handleClick(/* event */) {
+  const input = document.getElementById("text");
+  const note = input.value;
+  if (note) {
+    const list = document.getElementById("list");
+    const item = document.createElement("li");
+    item.textContent = note;
+    list.appendChild(item);
+  }
+}
