a724733e6dc19c7265da14f6ce58aa956d1667d5
saragojic <sa20b004@technikum-wien.at>
2022-10-09 21:39:18 +0200

    Step 34

--- script.js
+++ script.js
@@ -25,6 +25,7 @@ let notes = [
     const button = document.createElement("button");
 
     button.classList.add("note__controls__delete");
+    button.title = "Delete Note";
     button.addEventListener("click", handleClickDelete(note.id));
     const icon = document.createElement("i");
     icon.classList.add("fas", "fa-trash", "fa-2x");
